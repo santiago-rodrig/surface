d2197837dfeab8f163864ac6287ac7decac2667e
santiago-rodrig <santo1996.29@gmail.com>
2021-03-04 09:51:20 -0400

    first implementation with colors

--- main.go
+++ main.go
@@ -1,5 +1,93 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
 func main() {
-	println("not implemented")
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' hight='%d'>", width, height)
+
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, az := corner(i+1, j)
+			bx, by, bz := corner(i, j)
+			cx, cy, cz := corner(i, j+1)
+			dx, dy, dz := corner(i+1, j+1)
+
+			if isSomeNumberInvalid(az, bz, cz, dz) {
+				continue
+			}
+
+			maxZ := maxValue(az, bz, cz, dz) * zscale
+			var redValue int
+			var blueValue int
+
+			if maxZ < 0 {
+				blueValue = int(math.Abs(maxZ) * 25)
+			} else if maxZ > 0 {
+				redValue = int(maxZ * 25)
+			}
+
+			fmt.Printf("<polygon fill='rgb(%d, 0, %d)' points='%g,%g %g,%g %g,%g %g,%g' />\n", redValue, blueValue, ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+
+	fmt.Println("</svg>")
+}
+
+func isSomeNumberInvalid(numbers ...float64) bool {
+	for _, n := range numbers {
+		if math.IsNaN(n) || math.IsInf(n, 1) || math.IsInf(n, 0) || math.IsInf(n, -1) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func maxValue(values ...float64) float64 {
+	max := values[0]
+
+	for _, v := range values {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
+
+func corner(i, j int) (float64, float64, float64) {
+	// Find point (x, y) at corner of cell (i, j)
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z
+	z := f(x, y)
+
+	// Project (x, y, z) isometrically onto 2-D SVG canvas (sx, sy)
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+
+	return sx, sy, z
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0, 0)
+
+	return math.Sin(r) / r
 }
